ea58006152f16740e6d5fdc7e63f0da5a75695d9
alejandra38-o <alejandraobregonrojo@gmail>
2025-12-05 13:48:25 +0100

    prueba

---
+++ 4/prueba1.js
@@ -0,0 +1,16 @@
+ //soy un comentario; console.log es como un print.
+/* alert ventana que te salta un mensaje.......*/
+alert("prueba de código");
+var mensaje = "hola mundo que tal";
+console.log(mensaje);
+console.log(document);
+console.log(localStorage);
+
+
+var esViernes = true;
+console.log("Es viernes y queremos irnos: " + esViernes);
+//debugger
+if(esViernes == true){
+    console.log("Pues sí que es viernes, nos quedan 45 min para irnos");
+
+}
